4042fb324fbe1f729fe07c4f6f84258dd44eb62f
Veer Shah <veer.shah1016@gmail.com>
2020-03-30 10:18:47 -0400

    initial commit

---
+++ bio.py
@@ -0,0 +1,49 @@
+import pip._vendor.requests as requests
+from datetime import date, timedelta, datetime
+import json
+
+
+def getEntries(days):
+    #establish web session
+    session = requests.session()
+
+    #set constants
+    site_url = "http://www.biopharmcatalyst.com/calendars/pdufa-calendar"
+    headers = {
+        'Host': 'www.biopharmcatalyst.com',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:74.0) Gecko/20100101 Firefox/74.0',
+        'Referer': 'https://www.biopharmcatalyst.com/',
+    }
+
+    #get cookie to aviod csrf
+    source_code = session.get(site_url, headers=headers)
+    headers['Cookie'] = str({**session.cookies.get_dict('www.biopharmcatalyst.com'), **session.cookies.get_dict('.biopharmcatalyst.com')})
+
+    #get site source code
+    source_code = requests.get(site_url, headers=headers).text.split("\n")
+
+    #get all pdufa events from the source code
+    pdufaEvents = source_code[1856].split('= ')[1]
+    pdufaReviewEvents = source_code[1857].split('= ')[1]
+        #advisoryCommitteeDates = source_code[1858].split('= ')[1]
+
+    #parse to json
+    pdufaEvents = json.loads(pdufaEvents)
+    pdufaReviewEvents = json.loads(pdufaReviewEvents)
+
+    allEvents = pdufaEvents + pdufaReviewEvents
+
+    #get system date to locate next three months data
+    start_date = date.today()
+    end_date = start_date + timedelta(days)
+
+    #get all entires withing the date range and parse then to print into a csv
+    entires_in_date_range = []
+    for item in allEvents:
+        if start_date <= datetime.strptime(item['date'], '%Y-%m-%d').date() <= end_date:
+            entires_in_date_range.append(item)
+    return entires_in_date_range
+    
+#1856 pdufaEvents
+#1857 pdufaReviewEvents
+#1858 advisoryCommitteeDates
---
+++ main.py
@@ -0,0 +1,38 @@
+import bio
+import yahoo
+#libraries needed to make and open csv files
+import csv, platform, os
+from datetime import datetime
+
+#establishing the fields in my spreadsheet
+fields = ["Ticker", "Date", "Price", "Type", "Drug Name", "Note", "Market Cap",	"Yearly High", "Yearly Low"	,"Target", "Revenue", "Cash", "Debt", "Net Income Avai.",]
+
+entries = bio.getEntries(90)
+
+#this iterates through every entry and maps it to the information for that line on the csv
+#keeping a count for progress bar
+rows = []
+count = 0
+entries_len = len(entries)
+for entry in entries:
+    for i in range(len(entry['companies'])):
+        ticker = entry['companies'][i]['ticker']
+        yahoo_data = yahoo.scrape(ticker)
+        rows.append([ticker, entry['date'], round(entry['companies'][i]["price"], 2), entry['class'], entry['name'], entry['note'], yahoo_data[0], yahoo_data[2], yahoo_data[3], yahoo_data[1], yahoo_data[4], yahoo_data[5], yahoo_data[6], yahoo_data[7], ])
+    count += 1
+    print('Fetched data {}/{} done.'.format(count, entries_len))
+
+#write to csv file
+filename = "curated_list_" + datetime.today().strftime('%Y-%m-%d') + ".csv"
+
+print("\nwriting to csv file")
+with open(filename, 'w+') as csvfile: 
+    csvwriter = csv.writer(csvfile)  
+    csvwriter.writerow(fields)  
+    csvwriter.writerows(rows) 
+
+#open csv file
+print("\nopening file now")
+platform_name = platform.system()
+if platform_name == "Windows" : os.startfile(filename)
+if platform_name == "Darwin": os.system("open {}".format(filename))
