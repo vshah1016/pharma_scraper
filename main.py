56a560f493e03cc94fdcf2208fc842aff48c098e
Veer Shah <veer.shah1016@gmail.com>
2020-03-30 12:01:49 -0400

    add a progress bar, made everything look cleaner when te program is run

--- main.py
+++ main.py
@@ -1,38 +1,52 @@
 import bio
 import yahoo
 #libraries needed to make and open csv files
-import csv, platform, os
+import csv, platform, os, sys
 from datetime import datetime
+
+#progress bar code
+def progressbar(it, prefix="", size=60, file=sys.stdout):
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
+        file.flush()        
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+    file.write("\n")
+    file.flush()
 
 #establishing the fields in my spreadsheet
 fields = ["Ticker", "Date", "Price", "Type", "Drug Name", "Note", "Market Cap",	"Yearly High", "Yearly Low"	,"Target", "Revenue", "Cash", "Debt", "Net Income Avai.",]
 
 entries = bio.getEntries(90)
 
-#this iterates through every entry and maps it to the information for that line on the csv
-#keeping a count for progress bar
+#makes a new line so everything looks cleaner
+print("\n")
+
+#rows will eventually hold all data necessary
 rows = []
-count = 0
-entries_len = len(entries)
-for entry in entries:
+#this iterates through every entry and maps it to the information for that line on the csv
+#the first for loop goes through all of the entries but also makes a progeress bar for us
+for entry in progressbar(entries, "Fetching: "):
     for i in range(len(entry['companies'])):
         ticker = entry['companies'][i]['ticker']
         yahoo_data = yahoo.scrape(ticker)
         rows.append([ticker, entry['date'], round(entry['companies'][i]["price"], 2), entry['class'], entry['name'], entry['note'], yahoo_data[0], yahoo_data[2], yahoo_data[3], yahoo_data[1], yahoo_data[4], yahoo_data[5], yahoo_data[6], yahoo_data[7], ])
-    count += 1
-    print('Fetched data {}/{} done.'.format(count, entries_len))
 
 #write to csv file
 filename = "curated_list_" + datetime.today().strftime('%Y-%m-%d') + ".csv"
 
-print("\nwriting to csv file")
+print("\nWriting to csv file.")
 with open(filename, 'w+') as csvfile: 
     csvwriter = csv.writer(csvfile)  
     csvwriter.writerow(fields)  
     csvwriter.writerows(rows) 
 
 #open csv file
-print("\nopening file now")
+print("\nOpening file now.\n")
 platform_name = platform.system()
 if platform_name == "Windows" : os.startfile(filename)
 elif platform_name == "Darwin": os.system("open {}".format(filename))
