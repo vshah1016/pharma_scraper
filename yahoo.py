b0c1c2c73b055c221be1c1fd78dd57588f077aa1
Veer Shah <veer.shah1016@gmail.com>
2020-03-31 21:47:32 -0400

    somtimes yahoo did not return data so i fixed that because when nothing comes i just fetch the data again

--- yahoo.py
+++ yahoo.py
@@ -19,37 +19,40 @@ def text_to_num(text):
 
 
 def scrape(ticker):
-    #defining the stats we want to scrpe
-    key_stats_on_main =['Market Cap', '1y Target Est',]
-    key_stats_on_stat =['52 Week High', '52 Week Low', 'Revenue', 'Total Cash', 'Total Debt', 'Net Income Avi to Common',]
+    try:
+        #defining the stats we want to scrpe
+        key_stats_on_main =['Market Cap', '1y Target Est',]
+        key_stats_on_stat =['52 Week High', '52 Week Low', 'Revenue', 'Total Cash', 'Total Debt', 'Net Income Avi to Common',]
 
-    #the url for both of the different screens we want to scrape from
-    summary_page_url = "https://finance.yahoo.com/quote/{0}?p={0}".format(ticker)
-    statistics_page_url = "https://finance.yahoo.com/quote/{0}/key-statistics?p={0}".format(ticker)
+        #the url for both of the different screens we want to scrape from
+        summary_page_url = "https://finance.yahoo.com/quote/{0}?p={0}".format(ticker)
+        statistics_page_url = "https://finance.yahoo.com/quote/{0}/key-statistics?p={0}".format(ticker)
 
-    #array that will eventually hold all data wanted
-    #will be held in the following order
-    #[market cap, target, yearly high, yearly low, revenue, cash, debt, net income]
-    scraped_data = []
+        #array that will eventually hold all data wanted
+        #will be held in the following order
+        #[market cap, target, yearly high, yearly low, revenue, cash, debt, net income]
+        scraped_data = []
 
-    #getting html data from the summary page of the stock and parsing it with BeautifulSoup
-    soup = BeautifulSoup(requests.get(summary_page_url).text, 'html.parser')
+        #getting html data from the summary page of the stock and parsing it with BeautifulSoup
+        soup = BeautifulSoup(requests.get(summary_page_url).text, 'html.parser')
 
-    #iterating through all wanted stats and looking at that row, finding the parent tags and finding the value then it gets appended to the list
-    for stat in key_stats_on_main:
-        scraped_data.append(text_to_num(soup.find(text=stat).find_parent('tr').find_all('td')[1].contents[0].contents[0]))
+        #iterating through all wanted stats and looking at that row, finding the parent tags and finding the value then it gets appended to the list
+        for stat in key_stats_on_main:
+            scraped_data.append(text_to_num(soup.find(text=stat).find_parent('tr').find_all('td')[1].contents[0].contents[0]))
 
-    #parsing the statistics page of the ticker
-    soup = BeautifulSoup(requests.get(statistics_page_url).text, 'html.parser')
+        #parsing the statistics page of the ticker
+        soup = BeautifulSoup(requests.get(statistics_page_url).text, 'html.parser')
 
-    #iterating through all wanted stats and looking at that row, finding the parent tags and finding the value then it gets appended to the list
-    #we have to catch all of the N/A for the data that Yahoo Finance does not have on hand
-    for stat in key_stats_on_stat:
-        value = soup.find(text=stat).find_parent('td').find_parent('tr').find_all('td')[1].contents[0]
-        try:
-            scraped_data.append(value.contents[0])
-        except:
-            scraped_data.append(text_to_num(value))
+        #iterating through all wanted stats and looking at that row, finding the parent tags and finding the value then it gets appended to the list
+        #we have to catch all of the N/A for the data that Yahoo Finance does not have on hand
+        for stat in key_stats_on_stat:
+            value = soup.find(text=stat).find_parent('td').find_parent('tr').find_all('td')[1].contents[0]
+            try:
+                scraped_data.append(value.contents[0])
+            except:
+                scraped_data.append(text_to_num(value))
 
-    #returns the data that we have scraped from yahoo finance
-    return scraped_data
+        #returns the data that we have scraped from yahoo finance
+        return scraped_data
+    except:
+        return scrape(ticker)
